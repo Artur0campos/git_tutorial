f5ee889bf8e327517db74b6d3bfc288a4d2d3c03
Artur0campos <arturcamposcamara@gmail.com>
2024-02-05 10:32:10 -0300

    adicionado um do while loop

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
 int main(){
-    for(int x = 1;  x < 11; x ++){
+    int x = 1;
+    do{
+        for(int x = 1;  x < 11; x ++){
         std::cout << x << std::endl;
     }
     std::cout << "hello world";
+    return x = 11;
+    } while(x < 11);
 
     return 0;
 }
